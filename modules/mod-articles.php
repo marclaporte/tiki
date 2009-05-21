marclaporte/tiki

commit f0a0e66d78ab98ec69cae61c70a24432867b24d2

[ENH] add "offset" parameter so article list doesnt have to start at first one.
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/3.0@17911 b456876b-0849-0410-b77d-98878d47e9d5

modules/mod-last_articles.php
@@ -1,6 +1,17 @@
 <?php
 // $Id$
 
+// Copyright (c) 2002-2009, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+/**
+ * Show recent articles.
+ *
+ * 2009-04-10	SEWilco
+ * Add 'offset' parameter in case starting at zero is not wanted.
+ **/
+
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
@@ -31,8 +42,10 @@ if (isset($module_params['showImg'])) {
 if (isset($module_params['showDate']) && $module_params['showDate'] == 'y') {
 	$smarty->assign('showDate','y');
 }
+$offset = isset($module_params['offset']) ? $module_params['offset']+0 : '0';
+$smarty->assign('offset',$offset);
 
-$ranking = $tikilib->list_articles(0,$module_rows,'publishDate_desc', '', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
+$ranking = $tikilib->list_articles($offset,$module_rows,'publishDate_desc', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
 if (isset($module_params['showHeading']) && $module_params['showHeading'] != 'n') {
 	if ($module_params['showHeading'] == 'y')
 		$module_params['showHeading'] = -1;
@@ -47,3 +60,4 @@ $smarty->assign('absurl', isset($module_params["absurl"]) ? $module_params["absu
 $module_rows = count($ranking["data"]);
 $smarty->assign('module_rows', $module_rows);
 
+?>

commit 22593e987d6353571e0b09319899e5be75c3ae14

[bp/r18779][MOD] button plugin......end of the deployment. [bp/r18886][FIX] mod last articles: remove offset param from smarty to avoid some bad side effects on all pagination_links when ajax is active (broken in r17911)
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/proposed@18999 b456876b-0849-0410-b77d-98878d47e9d5

modules/mod-articles.php
@@ -6,46 +6,27 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 }
 global $smarty, $tikilib, $user;
 
-$smarty->assign('module_title', isset($module_params["title"]) ? $module_params["title"] : tra("Articles"));
+$smarty->assign('module_title', isset($module_params['title']) ? $module_params['title'] : tra('Articles'));
+$urlParams = array(
+	'topicId' => 'topic',
+	'topic' => 'topicName',
+	'categId' => 'categId',
+	'type' => 'type',
+	'lang' => 'lang',
+	'start' => null,
+	'sort' => null
+);
 
-if (isset($module_params["type"])) {
-	$type = $module_params["type"];
-} else {
-	$type = '';
+foreach ( $urlParams as $p => $v ) {
+	if ( isset($$p) ) continue;
+	$$p = isset($module_params[$p]) ? $module_params[$p] : '';
 }
-if (isset($module_params["topicId"])) {
-	$topicId = $module_params["topicId"];
-} else {
-	$topicId = '';
-}
-if (isset($module_params["topic"])) {
-	$topic = $module_params["topic"];
-} else {
-	$topic = '';
-}
-if (isset($module_params["start"])) {
-	$start = $module_params["start"];
-} else {
-	$start = isset($start) ? $start : 0;
-}
-if (isset($module_params['sort'])) {
-	$sort = $module_params['sort'];
-} else {
-	$sort = 'publishDate_desc';
-}
-if (isset($module_params['lang'])) {
-	$lang = $module_params['lang'];
-} else {
-	$lang = '';
-}
-if (isset($module_params['categId'])) {
-	$categId = $module_params['categId'];
-} else {
-	$categId = '';
-}
+if ( $start == '' ) $start = 0;
+if ( $sort == '' ) $sort = 'publishDate_desc';
 
 $ranking = $tikilib->list_articles($start, $module_rows, $sort, '', '', '', $user, $type, $topicId, 'y', $topic, $categId, '', '', $lang);
 
+$smarty->assign_by_ref('urlParams', $urlParams);
 $smarty->assign('modArticles', $ranking["data"]);
 if (isset($module_params['title'])) {
 	$smarty->assign('tpl_module_title', $module_params['title']);

modules/mod-last_articles.php
@@ -8,8 +8,6 @@
 /**
  * Show recent articles.
  *
- * 2009-04-10	SEWilco
- * Add 'offset' parameter in case starting at zero is not wanted.
  **/
 
 //this script may only be included - so its better to die if called directly.
@@ -27,37 +25,39 @@ global $tikilib, $smarty;
 // Parameter absurl set if the last_article url is absolute or not [y|n].
 // If not set, default = relative
 
-// filter for type, topicId or topic...
-// if ! is in front of type or topic, the result is inversed
-$mod_type = isset($module_params["type"]) ? $module_params["type"] : '';
-$mod_topicId = isset($module_params["topicId"]) ? $module_params["topicId"] : '';
-$mod_topic = isset($module_params["topic"]) ? $module_params["topic"] : '';
-$smarty->assign('type',$mod_type);
-$smarty->assign('topicId',$mod_topicId);
-$categId = isset($module_params['categId']) ? $module_params['categId'] : '';
-$l = isset($module_params['lang']) ? $module_params['lang'] : '';
-if (isset($module_params['showImg'])) {
-	$smarty->assign('showImg', $module_params['showImg']);
-}
-if (isset($module_params['showDate']) && $module_params['showDate'] == 'y') {
-	$smarty->assign('showDate','y');
-}
-$offset = isset($module_params['offset']) ? $module_params['offset']+0 : '0';
-$smarty->assign('offset',$offset);
+$urlParams = array(
+	'topicId' => 'topic',
+	'topic' => 'topicName',
+	'categId' => 'categId',
+	'type' => 'type',
+	'lang' => 'lang',
+	'showImg' => NULL,
+	'showDate' => NULL,
+	'showHeading' => NULL,
+	'nonums' => NULL,
+	'absurl' => NULL
+);
 
-$ranking = $tikilib->list_articles($offset,$module_rows,'publishDate_desc', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
-if (isset($module_params['showHeading']) && $module_params['showHeading'] != 'n') {
-	if ($module_params['showHeading'] == 'y')
-		$module_params['showHeading'] = -1;
-	$smarty->assign('showHeading',$module_params['showHeading']);
-	foreach ($ranking['data'] as $key=>$article) {
+foreach ( $urlParams as $p => $v ) {
+	if ( isset($$p) ) continue;
+	$$p = isset($module_params[$p]) ? $module_params[$p] : '';
+}
+
+$offset = (int) $module_params['offset'];
+if ( $absurl == '' ) $absurl = 'n';
+if ( $nonums == '' ) $nonums = 'n';
+if ( $showHeading != 'n') {
+	if ( $showHeading == 'y' ) $showHeading = -1;
+	foreach ( $ranking['data'] as $key => $article ) {
 		$ranking['data'][$key]['parsedHeading'] = $tikilib->parse_data($article['heading']);
 	}
 }
-$smarty->assign('modLastArticles',$ranking["data"]);
-$smarty->assign('nonums', isset($module_params["nonums"]) ? $module_params["nonums"] : 'n');
-$smarty->assign('absurl', isset($module_params["absurl"]) ? $module_params["absurl"] : 'n');
-$module_rows = count($ranking["data"]);
-$smarty->assign('module_rows', $module_rows);
 
-?>
+foreach ( $urlParams as $p => $v ) $smarty->assign($p, $$p);
+
+$ranking = $tikilib->list_articles($offset, $module_rows, 'publishDate_desc', '', date("U"), '', $type, $topicId, 'y', $topic, $categId, '', '', $lang);
+
+
+$module_rows = count($ranking['data']);
+$smarty->assign('module_rows', $module_rows);
+$smarty->assign('modLastArticles', $ranking['data']);

templates/modules/mod-articles.tpl
@@ -14,8 +14,14 @@
  {if $module_params.nonums != 'y'}</ol>{else}</ul>{/if}
 {if $module_params.more eq 'y'}
 	<div class="more">
-		 {assign var=sep value='?'}
-		 <span class="button2"><a href="tiki-view_articles.php{if $module_params.topicId}{$sep}topic={$module_params.topicId}{assign var=sep value='&amp;'}{/if}{if $module_params.topic}{$sep}topicName={$module_params.topic|escape:url}{assign var=sep value='&amp;'}{/if}{if $module_params.categId}{$sep}categId={$module_params.categId}{assign var=sep value='&amp;'}{/if}{if $module_params.type}{$sep}type={$module_params.type|escape:url}{assign var=sep value='&amp;'}{/if}{if $module_params.lang}{$sep}lang={$module_params.lang|escape:url}{assign var=sep value='&amp;'}{/if}">{tr}More...{/tr}</a></span>
+		{assign var=queryArgs value=''}
+		{foreach from=$urlParams item=urlParam key=urlParamKey}
+			{if !empty($urlParam) and !empty($module_params[$urlParamKey])}
+				{if empty($queryArgs)}{assign var=queryArgs value='?'}{else}{assign var=queryArgs value="$queryArgs&amp;"}{/if}
+				{assign var=queryArgs value="$queryArgs$urlParam=`$module_params[$urlParamKey]`"}
+			{/if}
+		{/foreach}
+		{button href="tiki-view_articles.php$queryArgs" _text="{tr}More...{/tr}"}
 	</div>
 {/if}
 {/tikimodule}

templates/modules/mod-last_articles.tpl
@@ -41,8 +41,14 @@
 {if $nonums != 'y'}</ol>{else}</ul>{/if}
 {if $module_params.more eq 'y'}
 	<div class="more">
-		 {assign var=sep value='?'}
-		 <span class="button2"><a href="tiki-view_articles.php{if $module_params.topicId}{$sep}topic={$module_params.topicId}{assign var=sep value='&amp;'}{/if}{if $module_params.topic}{$sep}topicName={$module_params.topic|escape:url}{assign var=sep value='&amp;'}{/if}{if $module_params.categId}{$sep}categId={$module_params.categId}{assign var=sep value='&amp;'}{/if}{if $module_params.type}{$sep}type={$module_params.type|escape:url}{assign var=sep value='&amp;'}{/if}{if $module_params.lang}{$sep}lang={$module_params.lang|escape:url}{assign var=sep value='&amp;'}{/if}">{tr}More...{/tr}</a></span>
+		{assign var=queryArgs value=''}
+		{foreach from=$urlParams item=urlParam key=urlParamKey}
+			{if !empty($urlParam) and !empty($module_params[$urlParamKey])}
+				{if empty($queryArgs)}{assign var=queryArgs value='?'}{else}{assign var=queryArgs value="$queryArgs&amp;"}{/if}
+				{assign var=queryArgs value="$queryArgs$urlParam=`$module_params[$urlParamKey]`"}
+			{/if}
+		{/foreach}
+		{button href="tiki-view_articles.php$queryArgs" _text="{tr}More...{/tr}"}
 	</div>
 {/if}
 {/tikimodule}