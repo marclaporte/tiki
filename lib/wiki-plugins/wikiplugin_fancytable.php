marclaporte/tiki

commit c903c821decc685b92365a85b6e6817bfda96690

[FIX] Need to mask tiki tag content (like ~np~ content ~/np~) in all cases, not just when cell divider is |, otherwise tag content could be mistaken for cell divider
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/7.x@34417 b456876b-0849-0410-b77d-98878d47e9d5

lib/wiki-plugins/wikiplugin_fancytable.php
@@ -87,9 +87,9 @@ function wikiplugin_fancytable($data, $params) {
 	// Start the table
 	$wret = '<table class="normal'.($sortable=='y'? ' fancysort':'').'" id="fancytable_'.$iFancytable.'">' . "\r\t";
 	
-	//if | is used as separator, mask tiki tags during processing and bring back at the end so that any pipes (|) 
-	//aren't mistaken for cell dividers
-	//pattern covers (( )), [ ], ~np~ /~np~, ~tc~ /~tc~, ~hc~ /~hc~, { = } (plugins with parameters)
+	//mask tiki tag content during processing and bring back at the end so that any pipes (| or ~|~) 
+	//inside of tags aren't mistaken for cell dividers
+	//pattern covers (( )), [ ], ~np~ ~/np~, ~tc~ ~/tc~, ~hc~ ~/hc~, { = } (plugins with parameters)
 	$pattern = '/(\(\([^\)\)]+\)\)|\[[^\]]+\]|~np~(?:(?!~\/np~).)*~\/np~|~tc~(?:(?!~\/tc~).)*~\/tc~'
 				. '|~hc~(?:(?!~\/hc~).)*~\/hc~|\{[^\=]+[\=]+[^\=\}]+\})/';
 	//process header
@@ -99,50 +99,36 @@ function wikiplugin_fancytable($data, $params) {
 		} else {
 			$tdhdr = "\r\t\t\t<th";
 		}
-		$separator = strpos($head, '~|~') === false ? '|' : '~|~';
-		//skip the preg matching if ~|~ is used
-		if ($separator != '~|~') {
-			preg_match_all($pattern, $head, $head_matches);
-			//replace all tiki tags in the header with numbered strings while being processed
-			$head = preg_replace_callback($pattern, 'replace_head', $head);
-		}
+		preg_match_all($pattern, $head, $head_matches);
+		//replace all tiki tags in the header with numbered strings while being processed
+		$head = preg_replace_callback($pattern, 'replace_head', $head);
 		//process header rows
-		$headrows = process_section($head, 'h', $separator, '>>', $tdhdr, '</th>', isset($colwidths) ? $colwidths : '', 
+		$headrows = process_section($head, 'h', '>>', $tdhdr, '</th>', isset($colwidths) ? $colwidths : '', 
 					isset($headaligns) ? $headaligns : '', isset($headvaligns) ? $headvaligns : '');
-		//skip the preg matching if ~|~ is used
-		if ($separator != '~|~') {
-			//bring the tiki tags back into the header. static veriable needed in case of multiple tables
-			static $hh = 0;
-			foreach ($head_matches[0] as $head_match) {
-				$headrows = str_replace('~~~head' . $hh . '~~~', $head_match, $headrows);
-				$hh++;
-			}
+		//bring the tiki tags back into the header. static veriable needed in case of multiple tables
+		static $hh = 0;
+		foreach ($head_matches[0] as $head_match) {
+			$headrows = str_replace('~~~head' . $hh . '~~~', $head_match, $headrows);
+			$hh++;
 		}
 		$wret .= '<thead>' . $headrows . "\r\t" . '</thead>' . "\r\t" . '<tbody>';
 	}
 	//process body
-	$separator = strpos($data, '~|~') === false ? '|' : '~|~';
-	//skip the preg matching if ~|~ is used
-	if ($separator != '~|~') {
-		preg_match_all($pattern, $data, $body_matches);
-		//replace all tiki tags in the body with numbered strings while being processed
-		$data = preg_replace_callback($pattern, 'replace_body', $data);
-	}
+	preg_match_all($pattern, $data, $body_matches);
+	//replace all tiki tags in the body with numbered strings while being processed
+	$data = preg_replace_callback($pattern, 'replace_body', $data);
 	//process table body rows
-	$bodyrows = process_section($data, 'r', $separator, "\n", '', '</td>', isset($colwidths) ? $colwidths : '', 
+	$bodyrows = process_section($data, 'r', "\n", '', '</td>', isset($colwidths) ? $colwidths : '', 
 				isset($colaligns) ? $colaligns : '', isset($colvaligns) ? $colvaligns : '');
-	//skip the preg matching if ~|~ is used
-	if ($separator != '~|~') {
-		//bring the tiki tags back into the body. static veriable needed in case of multiple tables
-		static $bb = 0;
-		foreach ($body_matches[0] as $body_match) {
-			$bodyrows = str_replace('~~~body' . $bb . '~~~', $body_match, $bodyrows);
-			$bb++;
-		}
+	//bring the tiki tags back into the body. static veriable needed in case of multiple tables
+	static $bb = 0;
+	foreach ($body_matches[0] as $body_match) {
+		$bodyrows = str_replace('~~~body' . $bb . '~~~', $body_match, $bodyrows);
+		$bb++;
 	}
 	$wret .= $bodyrows;
 
-	// End the table
+	//end the table
 	if (isset($head)) {
 		$wret .= "\r\t" . '</tbody>';
 	}
@@ -178,8 +164,8 @@ function replace_body($matches) {
 	return $ret;
 }
 //function to process header and body
-function process_section ($data, $type, $separator, $line_sep, $cellbeg, $cellend, $widths, $aligns, $valigns) {
-//	$separator = strpos($data, '~|~') === FALSE ? '|' : '~|~';
+function process_section ($data, $type, $line_sep, $cellbeg, $cellend, $widths, $aligns, $valigns) {
+	$separator = strpos($data, '~|~') === false ? '|' : '~|~';
 	$lines = explode($line_sep, $data);
 	$widths = !empty($widths) ?  explode('|', $widths) : '';
 	$aligns = !empty($aligns) ?  explode('|', $aligns) : '';

commit 075f20a8394f9c3b30cb3e87fad06197116c3d9b

[FIX] Pattern was messing up tables with complex syntax. Regression introduced with r34417.
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/7.x@35673 b456876b-0849-0410-b77d-98878d47e9d5

lib/wiki-plugins/wikiplugin_fancytable.php
@@ -89,9 +89,9 @@ function wikiplugin_fancytable($data, $params) {
 	
 	//mask tiki tag content during processing and bring back at the end so that any pipes (| or ~|~) 
 	//inside of tags aren't mistaken for cell dividers
-	//pattern covers (( )), [ ], ~np~ ~/np~, ~tc~ ~/tc~, ~hc~ ~/hc~, { = } (plugins with parameters)
+	//pattern covers (( )), [ ], ~np~ ~/np~, ~tc~ ~/tc~, ~hc~ ~/hc~, { }
 	$pattern = '/(\(\([^\)\)]+\)\)|\[[^\]]+\]|~np~(?:(?!~\/np~).)*~\/np~|~tc~(?:(?!~\/tc~).)*~\/tc~'
-				. '|~hc~(?:(?!~\/hc~).)*~\/hc~|\{[^\=]+[\=]+[^\=\}]+\})/';
+				. '|~hc~(?:(?!~\/hc~).)*~\/hc~|\{(?:(?!\}).)*\})/';
 	//process header
 	if (isset($head)) {
 		if (!empty($headclass)) {