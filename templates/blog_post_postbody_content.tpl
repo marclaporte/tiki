marclaporte/tiki

commit 8a579b055932747ee4992bfa09dced9316f28551

Added ability for blog plugin to work as a blog preview panel.
git-svn-id: file:///var/local/code.tiki.org/tikisvn/trunk@45750 b456876b-0849-0410-b77d-98878d47e9d5

templates/blog_post_postbody_content.tpl
@@ -1,7 +1,7 @@
 {* $Id$ *}
-{if $blog_post_context eq 'view_blog' && $use_excerpt eq 'y' && !empty($post_info.parsed_excerpt)}
+{if $blog_post_context eq 'view_blog' && $use_excerpt eq 'y' && !empty($post_info.excerpt)}
 	<div class="postbody-content postbody-excerpt">
-	{$post_info.parsed_excerpt}
+	{$post_info.excerpt}
 	</div>
 	{self_link _script=$post_info.postId|sefurl:blogpost _noauto='y'}{tr}Read more{/tr}{/self_link}
 {else}
@@ -27,12 +27,15 @@
 	</div>
 {/if}
 
-{capture name='copyright_section'}
-	{include file='show_copyright.tpl'}
-{/capture}
-{* When copyright section is not empty show it *}
-{if $smarty.capture.copyright_section neq ''}
-	<footer class="editdate">
-		{$smarty.capture.copyright_section}
-	</footer>
+{* Copyright display is being turned off if being called through the "BLOG" plugin with "simple" mode turned off and a max character count supplied (preview mode). If in preview mode end user most likely is wantingto conserve space with a smaller display of information *}
+{if $blog_post_context neq 'plugin_preview'}
+	{capture name='copyright_section'}
+		{include file='show_copyright.tpl'}
+	{/capture}
+	{* When copyright section is not empty show it *}
+	{if $smarty.capture.copyright_section neq ''}
+		<footer class="editdate">
+			{$smarty.capture.copyright_section}
+		</footer>
+	{/if}
 {/if}

lib/wiki-plugins/wikiplugin_bloglist.php
@@ -45,6 +45,59 @@ function wikiplugin_bloglist_info()
 					array('text' => tra('No'), 'value' => 'n')
 				),
 			),
+                        'charCount' => array(
+                                'required' => false,
+                                'name' => tra('Char Count'),
+                                'description' => tra('Number of characters to display if not a simple list. (defaults to all)'),
+                                'filter' => 'digits',
+                                'parent' => array('name' => 'simpleList', 'value' => 'n'),
+                                'default' => ''
+                        ),
+                        'wordBoundary' => array(
+                                'required' => false,
+                                'name' => tra('Word Boundary'),
+                                'description' => tra('If not a simple list and Char Count is non-zero, then marking this as yes will break on word boundaries only.'),
+                                'default' => 'y',
+                                'options' => array(
+                                        array('text' => tra('Yes'), 'value' => 'y'),
+                                        array('text' => tra('No'), 'value' => 'n')
+                                ),
+                                'parent' => array('name' => 'simpleList', 'value' => 'n'),
+                        ),
+                        'ellipsis' => array(
+                                'required' => false,
+                                'name' => tra('Ellipsis'),
+                                'description' => tra('If not a simple list and Char Count is non-zero, then marking this as yes will put ellipsis (...) at end of text (default=y).'),
+                                'default' => 'y',
+                                'options' => array(
+                                        array('text' => '', 'value' => ''),
+                                        array('text' => tra('Yes'), 'value' => 'y'),
+                                        array('text' => tra('No'), 'value' => 'n')
+                                ),
+                                'parent' => array('name' => 'simpleList', 'value' => 'n'),
+                        ),
+                        'more' => array(
+                                'required' => false,
+                                'name' => tra('More'),
+                                'description' => tra('If not a simple list and Char Count is non-zero, then marking this as yes will put a More link to the full entry (default=y).'),
+                                'default' => 'y',
+                                'options' => array(
+                                        array('text' => tra('Yes'), 'value' => 'y'),
+                                        array('text' => tra('No'), 'value' => 'n')
+                                ),
+                                'parent' => array('name' => 'simpleList', 'value' => 'n'),
+                        ),
+                        'showIcons' => array(
+                                'required' => false,
+                                'name' => tra('Show Icons'),
+                                'description' => tra('If not a simple list marking this as no will prevent the "edit" and "print" type icons from displaying (default=y)'),
+                                'default' => 'y',
+                                'options' => array(
+					array('text' => tra('Yes'), 'value' => 'y'),
+                                        array('text' => tra('No'), 'value' => 'n')
+                                ),
+                                'parent' => array('name' => 'simpleList', 'value' => 'n'),
+                        ),
 			'dateStart' => array(
 				'required' => false,
 				'name' => tra('Start Date'),
@@ -111,10 +164,19 @@ function wikiplugin_bloglist($data, $params)
 		
 		$blogItems = $bloglib->list_blog_posts($params['Id'], false, $params['offset'], $params['Items'], $params['sort_mode'], $params['find'], $dateStartTS, $dateEndTS);
 
+		if ( $params['charCount'] > 0 )
+		{
+			$blogItems = $bloglib->mod_blog_posts( $blogItems, $params['charCount'], $params['wordBoundary'], $params['ellipsis'], $params['more']);
+		}
+
 		$blog_data = TikiLib::lib('blog')->get_blog($params['Id']);
 		$smarty->assign('blog_data', $blog_data);
 
 		$smarty->assign('ownsblog', $user && $user == $blog_data["user"] ? 'y' : 'n');
+
+		if ($params['showIcons'] == 'n') {
+                        $smarty->assign('excerpt', 'y');
+                }
 
 		$smarty->assign('show_heading', 'n');
 		$smarty->assign('use_author', 'y');

templates/blog_post_postbody_title.tpl
@@ -2,7 +2,9 @@
 <header class="clearfix postbody-title">
 	<div class="title">
 		{if $blog_post_context eq 'view_blog'}
-			<h2>{object_link type="blog post" id=$post_info.postId title=$post_info.title} {if $post_info.priv eq 'y'} ({tr}private{/tr}){/if}</h2>
+			<h2>{object_link type="blog post" id=$post_info.postId title=$post_info.title}{if $post_info.priv eq 'y'} ({tr}private{/tr}){/if}</h2>
+		{elseif $blog_post_context eq 'excerpt'}
+			<bold>{object_link type="blog post" id=$post_info.postId title=$post_info.title}</bold>
 		{else}
 			<h2>{$post_info.title|escape} {if $post_info.priv eq 'y'} ({tr}private{/tr}){/if}</h2>
 		{/if}

templates/blog_wrapper.tpl
@@ -4,12 +4,14 @@
 	{include file='blog_post_postbody_title.tpl'}
 	{include file='blog_post_author_info.tpl'}
 	{include file='blog_post_postbody_content.tpl'}
-	<div class="postfooter">
-		{if $blog_post_context ne 'print'}
-			{include file='blog_post_author_actions.tpl'}
-			{include file='blog_post_actions.tpl'}
-			{include file='blog_post_status.tpl'}
+	{if $blog_post_context ne 'excerpt'}
+		{if $blog_post_context ne 'print'} 
+			<div class="postfooter">
+				{include file='blog_post_author_actions.tpl'}
+				{include file='blog_post_actions.tpl'}
+				{include file='blog_post_status.tpl'}
+			</div>
 		{/if}
-	</div>
-	{include file='blog_post_navigation.tpl'}
+		{include file='blog_post_navigation.tpl'}
+	{/if}
 </div> <!-- postbody -->

templates/tiki-view_blog.tpl
@@ -52,9 +52,15 @@
 	{/if}
 {/if}
 
+{if $excerpt eq 'y'}
+	{assign "request_context" "excerpt"}
+{else}
+	{assign "request_context" "view_blog"}
+{/if}
+
 {foreach from=$listpages item=post_info}
 	<article class="blogpost post{if !empty($container_class)} {$container_class}{/if}">
-		{include file='blog_wrapper.tpl' blog_post_context='view_blog'}
+		{include file='blog_wrapper.tpl' blog_post_context=$request_context}
 	</article>
 {/foreach}
 

commit 21d78d206843faaf4b06168ff47181f45c5b08ef

[FIX] blogs: Display parsed_except not the source like in Tiki 11 (regression from r45750)
git-svn-id: file:///var/local/code.tiki.org/tikisvn/branches/12.x@50805 b456876b-0849-0410-b77d-98878d47e9d5

templates/blog_post_postbody_content.tpl
@@ -1,7 +1,7 @@
 {* $Id$ *}
 {if $blog_post_context eq 'view_blog' && $use_excerpt eq 'y' && !empty($post_info.excerpt)}
 	<div class="postbody-content postbody-excerpt">
-	{$post_info.excerpt}
+	{$post_info.parsed_excerpt}
 	</div>
 	{self_link _script=$post_info.postId|sefurl:blogpost _noauto='y'}{tr}Read more{/tr}{/self_link}
 {else}